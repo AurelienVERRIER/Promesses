28c443bfa764ff114a59365825f95cf41a2a526e
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-19 14:10:44 +0200

    Time out set

---
+++ index.js
@@ -0,0 +1,25 @@
+// const getHousesInParis = () => {
+//   // declaration de la promess
+//   return new Promise((resolve, reject) => {
+//     const houses = [];
+
+    setTimeout(() => {
+      "Hello world"
+    }, 2000);
+  // });
+// };
+
+// const waitForMyResults = async () => {
+//   try {
+//     // resolve
+//     console.log("loading...");
+//     const result = await getHousesInParis();
+//     console.log(result);
+//   } catch (error) {
+//     // reject
+//     console.log("error");
+//     console.log(error);
+//   }
+// };
+
+// waitForMyResults();
